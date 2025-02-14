1a7582dc28a794870d6a495841c3163f1f3ede96
youjin119 <youjin3012@naver.com>
2025-02-14 18:09:45 +0900

    edit:first_page

--- src/js/products.js
+++ src/js/products.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
   //캐러셀
   $(".slider").slick( {
     autoplay: true,
-    autoplaySpeed: 0, //1000ms = 1s 1초
+    autoplaySpeed: 0,
     speed:2000,
     cssEase:"linear",
-    slidesToShow: 5, //처음 보여줄 갯수
-    slidesToScroll: 1, //몇개씩 움직일지
-    pauseOnFocus: true, //마우스가 올라가면 멈출지 결정
-    pauseOnHover: true, // -
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    pauseOnFocus: true,
+    pauseOnHover: true,
     responsive: [ //반응형 설정
       // {
       //   breakpoint: 768,
