72c0da15c018651470dbb67a7a43f22b61618e03
youjin119 <youjin3012@naver.com>
2025-02-14 17:53:46 +0900

    add: first_page

--- src/js/products.js
+++ src/js/products.js
@@ -1,3 +1,28 @@
 $(document).ready(function(){
-  //여기부터 시작
+  //캐러셀
+  $(".slider").slick( {
+    autoplay: true,
+    autoplaySpeed: 0, //1000ms = 1s 1초
+    speed:2000,
+    cssEase:"linear",
+    slidesToShow: 5, //처음 보여줄 갯수
+    slidesToScroll: 1, //몇개씩 움직일지
+    pauseOnFocus: true, //마우스가 올라가면 멈출지 결정
+    pauseOnHover: true, // -
+    responsive: [ //반응형 설정
+      // {
+      //   breakpoint: 768,
+      //   settings: {
+      //     slidesToShow: 3,
+      //   }
+      // },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 3,                  
+        }
+      }
+    ]
+
+  } );
 });
